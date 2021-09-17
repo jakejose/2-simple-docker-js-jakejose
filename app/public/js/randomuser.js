e0485807ebfc6b522f89ac43a19be94c717f3d3d
Jacob Joseph <jakejose@iu.edu>
2021-09-16 22:09:38 -0400

    json data updates

--- app/public/js/randomuser.js
+++ app/public/js/randomuser.js
@@ -3,41 +3,35 @@ const SomeApp = {
     data() {
       return {
         result: {},
-        list: [5,6,7,8],
-        message: "Waiting ..."
+        dob: []
       }
     },
     created() {
 
-        //Method 1:
+        //fetching
         fetch('https://randomuser.me/api/')
         .then(response => response.json())
         .then((json) => {
             console.log("Got json back:", json);
             this.result = json.results[0];
+            this.dob=this.result.dob.date.slice(0,10)
         })
         .catch( (error) => {
             console.error(error);
-        });
-        /*
-            .then(response => response.json())
-        Is the same as
-            .then(function(response) {return response.json()})
-        */
-
-
-        //Method 2:
-        // const response = await fetch("https://randomuser.me/api/");
-        // const responseJson = await response.json();
-
-        // console.log("Two:", responseJson);
-        // this.message = responseJson.results[0].name;
-        // this.result = responseJson.results[0];
-        
+        });   
     }
 
   }
   
-  Vue.createApp(SomeApp).mount('#someApp');
+Vue.createApp(SomeApp).mount('#someApp');
+
+//refresh data attempt 1
+// function refresher (){
+//   let button = document.querySelector('#button')
+//   button.addEventListener('click',()=>{
+//     Vue.createApp(SomeApp).mount('#someApp');
+//   })
+// }
+
 
         
