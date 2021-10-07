8aca613a63aeebc68e971bf15dffc6d86b95bb6a
Jacob Joseph <jakejose@iu.edu>
2021-10-06 22:20:36 -0400

    schema update

--- app/data/schema.sql
+++ app/data/schema.sql
@@ -1,41 +1,20 @@
-CREATE DATABASE IF NOT EXISTS msisdb;
-USE msisdb;
+show databases;
+use newDB;
 
-DROP TABLE IF EXISTS student;
-CREATE TABLE student (
-	id int PRIMARY KEY AUTO_INCREMENT ,
-    username varchar(24) UNIQUE NOT NULL,
-    name varchar(48)
-);
+DROP TABLE IF EXISTS books;
+CREATE TABLE books (
+	id int PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    title varchar(255) NOT NULL,
+    author varchar(255) NOT NULL,
+    yearPublished int check (yearPublished between 0 and 2022),
+    publisher varchar(255),
+    pageCount int,
+    MRP varchar(20));
 
-INSERT INTO student (id, username, name) VALUES 
-(1, 'tomgreg', 'Tom Gregory'),
-(2, 'beth1', 'Beth Barnhart'),
-(3, 'bipin', 'Prof. Prabhakar');
 
--- SELECT * FROM students;
-
-DROP TABLE IF EXISTS offer;
-CREATE TABLE offer (
-	id int PRIMARY KEY AUTO_INCREMENT,
-    studentId int NOT NULL REFERENCES student(id) 
-        ON DELETE CASCADE ON UPDATE CASCADE,
-	companyName VARCHAR(24) NOT NULL DEFAULT '',
-    salary int NOT NULL DEFAULT 0,
-    bonus int NOT NULL DEFAULT 0,
-	offerDate date NOT NULL DEFAULT(CURRENT_DATE)
-);
-
--- Student 1 has no offers, Student 2 has 3 offers, Student 3 has one offer
-INSERT INTO offer(id, studentId, companyName, salary, bonus, offerDate) VALUES
-  (1, 2, 'KPMG', 95000, 7000, '2021-09-30'),
-  (2, 2, 'Deloitte Digital', 94000, 12000, '2021-10-03'),
-  (3, 2, 'IU, ISGP', 54000, 0, '2021-10-05'),
-  (4, 3, 'Amazon', 122000, 11000, '2021-10-15')
-;
-
--- COMMIT;
-
--- CREATE USER 'msisreader'@'%' IDENTIFIED BY 'msisreadonly';
--- GRANT SELECT ON * . * TO 'msisreader'@'%';
-
+INSERT INTO books (title, author, yearPublished, publisher, pageCount, MRP) VALUES 
+('The Godfather (The Book)', 'Mr. Godfather',1980,'	Godfather Publishing',300,'$2.00'),
+('How to Pass MSIS', 'Kelley School of Business',2000,'IU',2143,'$10.99'),
+('Intro to Github', 'Github Master',2015,'Githubiscool Books',123,'$5.00'),
+('Being a Doctor for Dummies', 'Dr. Lego',2020,'Dr. Lego (Self Published)',5000,'$100.00'),
+('Doctor 101', 'Dr. Lego Sr.',1980,'Lego Books',3405,'$91.99');
